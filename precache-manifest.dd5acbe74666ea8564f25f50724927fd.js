acbe3afb21d6262cedd7d4a13d8e555e2c44ff2f
Piotr Łysik <piotrek.lysik@gmail.com>
2019-11-04 12:30:59 +0100

    Updates

---
+++ precache-manifest.dd5acbe74666ea8564f25f50724927fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afe73a8e3f43a5e26c1a05070a591eb",
+    "url": "/web-view/index.html"
+  },
+  {
+    "revision": "8fd79bde20a66e7c4ba8",
+    "url": "/web-view/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0500fd91be33402d1e31",
+    "url": "/web-view/static/js/2.ebc2a20e.chunk.js"
+  },
+  {
+    "revision": "8fd79bde20a66e7c4ba8",
+    "url": "/web-view/static/js/main.479cc1a7.chunk.js"
+  },
+  {
+    "revision": "f98bc02201e0feb3cbc1",
+    "url": "/web-view/static/js/runtime-main.1d680c4c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/web-view/static/media/logo.25bf045c.svg"
+  }
+]);
